930f7e3c8a1f21823069893e27b82ecb14ac8605
dhimanshubham440818 <dhimanshubham440818@gmail.com>
2023-12-04 11:29:17 +0530

    env file added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const fs = require('fs');
+require('dotenv').config();
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 let users = [];
 //New imports
 const http = require('http').Server(app);
